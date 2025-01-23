162fb6fb181a298e0d812951e3edcb0a0453f8b0
JoroTop <GStoychev20@gmail.com>
2025-01-23 21:22:32 +0200

    RepairModal changes

--- src/components/RepairModal/RepairModal.jsx
+++ src/components/RepairModal/RepairModal.jsx
@@ -1,32 +1,29 @@
+import classes from "./RepairModal.module.css";
+
 export default function RepairModal() {
     return (
         <div>
-            <div>
-                <h2>Добави ремонт:</h2>
-                <div>
-                    <button>X</button>
-                </div>
-                <div>
-                    <div>Дана на ремонта:</div>
-                    <input type="date" />
-                </div>
-                <div>
-                    <div>Профилактика:</div>
-                    <input type="checkbox" />
-                </div>
-                <div>
-                    <div>Ремонтирана oт:</div>
-                    <input type="text" placeholder="Въведи име" />
-                </div>
+            <h2>Добави ремонт:</h2>
+            <div className={classes.sectionWrapper}>
+                <div>Дана на ремонта:</div>
+                <input type="date" />
             </div>
-            <div></div>
-            <div>
+            <div className={classes.sectionWrapper}>
+                <div>Профилактика:</div>
+                <input type="checkbox" />
+            </div>
+            <div className={classes.sectionWrapper}>
+                <div>Ремонтирана oт:</div>
+                <input type="text" placeholder="Въведи име" />
+            </div>
+            <div className={classes.sectionWrapper}>
                 <h3>Сменени части и други ремонти:</h3>
-                <textarea></textarea>
-                <div>
-                    <button>Запази</button>
-                    <button>Излез</button>
-                </div>
+            </div>
+            <textarea></textarea>
+
+            <div>
+                <button>Запази</button>
+                <button>Излез</button>
             </div>
         </div>
     );
