d6a7013c556aed854b0dd509166bcfc1d08768da
JoroTop <GStoychev20@gmail.com>
2025-06-15 16:13:39 +0300

    input and seraching serial

--- src/components/ProtocolModal/ProtocolPlus.jsx
+++ src/components/ProtocolModal/ProtocolPlus.jsx
@@ -1,14 +1,26 @@
-import { useState, useEffect, lazy } from "react";
+import { useState, useEffect, lazy, useRef } from "react";
 
 import { getDatabase, ref, onValue } from "firebase/database";
 
 const PartnerSection = lazy(() => import("./PartnerSection.jsx"));
 const PartnerModal = lazy(() => import("./PartnerModal.jsx"));
+const ErrorModal = lazy(() => import("../ErrorModal/ErrorModal.jsx"));
 const Menu = lazy(() => import("./ProtocolMenu.jsx"));
 import classes from "./ProtocolPlus.module.css";
 
 export default function ProtocolPlus({ company, toggleProtocol }) {
     const [listOfMachines, setListOfMachines] = useState([]);
+    const [selectedMachines, setSelectedMachines] = useState([]);
+    const [inputSerialNumber, setInputSerialNumber] = useState();
+    const [addingMachine, setAddingMachine] = useState(false);
+    const [error, setError] = useState(false);
+    const errorModal = useRef();
+
+    useEffect(() => {
+        if (error && errorModal.current) {
+            errorModal.current.open();
+        }
+    }, [error]);
 
     useEffect(() => {
         const database = getDatabase();
@@ -30,90 +42,144 @@ export default function ProtocolPlus({ company, toggleProtocol }) {
         return () => unsubscribe();
     }, []);
 
+    const handleSearchMachine = () => {
+        if (inputSerialNumber.trim() === "") {
+            setError(true);
+            return;
+        }
+
+        const findedMachine = listOfMachines.filter(
+            (m) => m.serialNumber === inputSerialNumber.trim()
+        );
+        if (findedMachine.length === 0) {
+            setError(true);
+        }
+        setSelectedMachines(findedMachine);
+    };
+
     return (
-        <div className={classes.protocolModal}>
-            <Menu closeProtocolmodal={toggleProtocol} />
-            <div>
-                <h1>Приемо-предавателен Протокол</h1>
-                <div className={classes.section}>
-                    <p>Дата: {new Date().toLocaleDateString("en-GB")}</p>
-                    <h2>Данни на страните</h2>
-                    <div className={classes.partyInfo}>
-                        <div className={classes.party}>
-                            <h3>Предаваща страна</h3>
-                            <p>{company.name}</p>
-                            <p>Мол:{company.mol}</p>
-                            <p>Адрес:{company.adress}</p>
-                            <p>Телефон:{company.phone}</p>
-                        </div>
+        <>
+            {error && (
+                <ErrorModal
+                    title="Няма такава машина!"
+                    text={
+                        "Въведиения сериен номер е грешен или машина с този номер не съществува"
+                    }
+                    setError={setError}
+                    ref={errorModal}
+                />
+            )}
 
-                        <div className={classes.party}>
-                            <h3>Приемаща страна</h3>
-                            <p>Ime</p>
-                            <p>Мол: </p>
-                            <p>Адрес: </p>
-                            <p>Телефон: </p>
-                        </div>
-                    </div>
-
+            <div className={classes.protocolModal}>
+                <Menu closeProtocolmodal={toggleProtocol} />
+                <div>
+                    <h1>Приемо-предавателен Протокол</h1>
                     <div className={classes.section}>
-                        <h2>Данни за предадената машина</h2>
-                        <table>
-                            <button>+ </button>
-                            <thead>
-                                <tr>
-                                    <th>Модел</th>
-                                    <th>Марка</th>
-                                    <th>Сериен номер</th>
-                                    <th>Действие</th>
-                                </tr>
-                            </thead>
-
-                            <tbody>
-                                {/* <tr>
-                                    <td>{machine.model}</td>
-                                    <td>{machine.brand}</td>
-                                    <td>{machine.serialNumber}</td>
-                                    <td>
-                                        <select
-                                            className={classes.movementInfo}
-                                        >
-                                            <option>Монтаж</option>
-                                            <option>Демонтаж</option>
-                                        </select>
-                                    </td>
-                                </tr> */}
-                            </tbody>
-                        </table>
-                    </div>
-
-                    <div className={classes.signaturesSection}>
-                        <h2>Подписи</h2>
-                        <div className={classes.signatures}>
-                            <div>
-                                <select className={classes.employee}>
-                                    <option>Георги Стойчев</option>
-                                    <option>Красимир Тюлиев</option>
-                                </select>
-                                <p>Подпис на предаващата страна:</p>
-                                <p>____________________________</p>
+                        <p>Дата: {new Date().toLocaleDateString("en-GB")}</p>
+                        <h2>Данни на страните</h2>
+                        <div className={classes.partyInfo}>
+                            <div className={classes.party}>
+                                <h3>Предаваща страна</h3>
+                                <p>{company.name}</p>
+                                <p>Мол:{company.mol}</p>
+                                <p>Адрес:{company.adress}</p>
+                                <p>Телефон:{company.phone}</p>
                             </div>
-                            <div>
-                                <p>Ime i familia</p>
-                                <p>Подпис на приемащата страна:</p>
-                                <p>____________________________</p>
+
+                            <div className={classes.party}>
+                                <h3>Приемаща страна</h3>
+                                <p>Ime</p>
+                                <p>Мол: </p>
+                                <p>Адрес: </p>
+                                <p>Телефон: </p>
+                            </div>
+                        </div>
+
+                        <div className={classes.section}>
+                            <h2>Данни за предадената машина</h2>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>Модел</th>
+                                        <th>Марка</th>
+                                        <th>Сериен номер</th>
+                                        <th>Действие</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {selectedMachines.map((machine) => (
+                                        <tr key={machine.id}>
+                                            <td>{machine.model}</td>
+                                            <td>{machine.brand}</td>
+                                            <td>{machine.serialNumber}</td>
+                                            <td>
+                                                <select
+                                                    className={
+                                                        classes.movementInfo
+                                                    }
+                                                >
+                                                    <option>Монтаж</option>
+                                                    <option>Демонтаж</option>
+                                                </select>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                            <div className={classes.addingContainer}>
+                                <button
+                                    onClick={() => {
+                                        setAddingMachine(!addingMachine);
+                                    }}
+                                >
+                                    +
+                                </button>
+                                {addingMachine && (
+                                    <div className={classes.inputWrapper}>
+                                        <input
+                                            type="number"
+                                            onChange={(e) =>
+                                                setInputSerialNumber(
+                                                    e.target.value
+                                                )
+                                            }
+                                        ></input>
+                                        <button onClick={handleSearchMachine}>
+                                            Добави
+                                        </button>
+                                    </div>
+                                )}
+                            </div>
+                        </div>
+
+                        <div className={classes.signaturesSection}>
+                            <h2>Подписи</h2>
+                            <div className={classes.signatures}>
+                                <div>
+                                    <select className={classes.employee}>
+                                        <option>Георги Стойчев</option>
+                                        <option>Красимир Тюлиев</option>
+                                    </select>
+                                    <p>Подпис на предаващата страна:</p>
+                                    <p>____________________________</p>
+                                </div>
+                                <div>
+                                    <p>Ime i familia</p>
+                                    <p>Подпис на приемащата страна:</p>
+                                    <p>____________________________</p>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <button
-                className={classes.printButton}
-                onClick={() => window.print()}
-            >
-                Принтирай протокола
-            </button>
-        </div>
+                <button
+                    className={classes.printButton}
+                    onClick={() => window.print()}
+                >
+                    Принтирай протокола
+                </button>
+            </div>
+        </>
     );
 }
