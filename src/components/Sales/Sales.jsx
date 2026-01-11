30a7e1497df9d2fc0ed04bd756045154b396cdf9
JoroTop <GStoychev20@gmail.com>
2026-01-11 22:34:07 +0200

    WIP Sales module

--- src/components/Sales/Sales.jsx
+++ src/components/Sales/Sales.jsx
@@ -1,9 +1,58 @@
+import styles from "../Sales/Sales.module.css";
+
 export default function Sales({ user, toggleSales }) {
     return (
         <>
-            <div>
-                <h1>Helloo from Sales</h1>
-                <p>{user.name}</p>
+            <div className={styles.wrapper}>
+                <div className={styles.nav}>
+                    <button>Добави клиент</button>
+                    <button>Продажба</button>
+                    <h1>Последни Продажби</h1>
+                </div>
+
+                <table className={styles.tableDocs}>
+                    <thead>
+                        <tr>
+                            <th>Дата </th>
+                            <th>Фирма</th>
+                            <th>Oбект</th>
+                            <th>Стока</th>
+                            <th>Обща стойност</th>
+                        </tr>
+                    </thead>
+                    {/* <tbody>
+                        {movementsWithMachineData &&
+                        movementsWithMachineData.length > 0 ? (
+                            movementsWithMachineData.map((move, index) => (
+                                <tr key={index}>
+                                    <td>
+                                        {new Date(move.date).toLocaleDateString(
+                                            "bg-BG"
+                                        )}
+                                    </td>
+                                    <td>{move.partner}</td>
+                                    <td>{move.machineBrand}</td>
+                                    <td>{move.machineModel}</td>
+                                    <td>{move.machineSerial}</td>
+                                    <td>{move.contact}</td>
+                                    <td>{move.object}</td>
+                                </tr>
+                            ))
+                        ) : (
+                            <tr>
+                                <td
+                                    colSpan="7"
+                                    style={{
+                                        textAlign: "center",
+                                        padding: "8px",
+                                    }}
+                                >
+                                    Няма налични ремонти
+                                </td>
+                            </tr>
+                        )}
+                    </tbody> */}
+                </table>
             </div>
         </>
     );
