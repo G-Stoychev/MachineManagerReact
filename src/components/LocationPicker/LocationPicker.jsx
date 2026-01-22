48437f627220b9268f3e834f0c891576f962b3d6
gStoychev <gstoychev20@gmail.com>
2026-01-22 18:55:04 +0200

    change some logic for LacationPicker

--- src/components/LocationPicker/LocationPicker.jsx
+++ src/components/LocationPicker/LocationPicker.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState, lazy } from "react";
 
+import styles from "./Map.module.css";
+
 const Map = lazy(() => import("./Map.jsx"));
 import { updateMachineCoords } from "../../services/dataService";
 
@@ -9,24 +11,11 @@ export default function LocationPicker({ machine }) {
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        if (!machine?.coords && !previewCoords) {
-            if (!navigator.geolocation) {
-                setError("Геолокацията не се поддържа");
-                return;
-            }
-
-            navigator.geolocation.getCurrentPosition(
-                (pos) => {
-                    const myCoords = {
-                        lat: pos.coords.latitude,
-                        lng: pos.coords.longitude,
-                    };
-                    setPreviewCoords(myCoords);
-                },
-                () => setError("Неуспешно взимане на локация"),
-                { enableHighAccuracy: true },
-            );
-        } else if (machine?.coords && !previewCoords) {
+        if (!navigator.geolocation) {
+            setError("Геолокацията не се поддържа");
+            return;
+        }
+        if (machine?.coords && !previewCoords) {
             setPreviewCoords(machine.coords);
         }
     }, [machine, previewCoords]);
@@ -47,33 +36,39 @@ export default function LocationPicker({ machine }) {
         );
     };
 
-    // 3. Save (commit)
     const handleSave = () => {
         setSavedCoords(previewCoords);
         updateMachineCoords(machine.id, previewCoords);
     };
 
-    // Проверка за промяна (dirty state)
     const isDirty =
         JSON.stringify(previewCoords) !== JSON.stringify(savedCoords);
 
     return (
         <div>
-            <h3>Локация на обекта</h3>
-
-            <Map value={previewCoords} onChange={setPreviewCoords} />
-
+            <h3 className={styles.h}>Локация на обекта</h3>
             <div style={{ marginTop: 10, display: "flex", gap: 10 }}>
-                <button onClick={handleResetToMyLocation}>
-                    🔄 Моята локация
+                <button
+                    onClick={handleResetToMyLocation}
+                    className={styles.btn}
+                >
+                    Моята локация
                 </button>
 
-                <button onClick={handleSave} disabled={!isDirty}>
-                    💾 Запази
+                <button
+                    className={styles.btn}
+                    onClick={handleSave}
+                    disabled={!isDirty}
+                >
+                    Запази
                 </button>
             </div>
 
-            {error && <p style={{ color: "red" }}>{error}</p>}
+            {error ? (
+                <p className={styles.errorP}>{error}</p>
+            ) : (
+                <Map value={previewCoords} onChange={setPreviewCoords} />
+            )}
         </div>
     );
 }
